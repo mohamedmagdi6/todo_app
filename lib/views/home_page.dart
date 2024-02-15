d9c01418b1467de88e9aaf31b34c468e8b65f233
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-02-16 01:11:31 +0200

    finshed bottonme navigator bar

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/archived_body.dart';
+import 'package:todo_app/widgets/done_body.dart';
+import 'package:todo_app/widgets/taske_body.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,17 +10,60 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+int currentIndex = 0;
+List<Widget> bodyList = [
+  const TasksBody(),
+  const DoneBody(),
+  const ArchivedBody()
+];
+
+List<String> titleTextList = ['Task Page', 'Done Page', 'Archived Page'];
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'todo app',
-          style: TextStyle(color: Colors.white),
+        title: Text(
+          titleTextList[currentIndex],
+          style: const TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.menu,
+              ),
+              label: 'Tasks'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.done,
+              ),
+              label: 'Done'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.archive,
+              ),
+              label: 'Archived'),
+        ],
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+      ),
+      body: bodyList[currentIndex],
     );
   }
 }
---
+++ lib/widgets/taske_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TasksBody extends StatelessWidget {
+  const TasksBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
